f779e852ea3cc84df747359df1e654bf978ecba7
Necxes <contact@patrick-stark.com>
2024-03-19 23:21:05 +0100

    added Model section and modelView

---
+++ src/components/Model.jsx
@@ -0,0 +1,29 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import ModelView from "./ModelView";
+
+const Model = () => {
+    useGSAP(() => {
+        gsap.to('#heading', {
+            opacity: 1, y: 0
+        })
+    })
+
+    return (
+        <section className="common-padding w-screen">
+            <div className="screen-max-width">
+                <h1 id="heading" className="section-heading">
+                    Take a closer look.
+                </h1>
+
+                <div className="flex flex-col-items-center mt5">
+                    <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
+                        <ModelView />
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Model
