9dc4978ef0e640d979efb4f599479743686fb202
Necxes <contact@patrick-stark.com>
2024-03-26 11:37:21 +0100

    fixed vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,24 +4,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "patrick-stark",
-    project: "javascript-react"
+  plugins: [
+    react(), 
+    sentryVitePlugin({
+      org: "patrick-stark",
+      project: "apple_copycat"
   })],
 
   build: {
     sourcemap: true
-  },
-  plugins: [
-    react(),
-
-    // Put the Sentry vite plugin after all other plugins
-    sentryVitePlugin({
-      org: process.env.SENTRY_ORG,
-      project: process.env.SENTRY_PROJECT,
-
-      // Auth tokens can be obtained from https://sentry.io/orgredirect/organizations/:orgslug/settings/auth-tokens/
-      authToken: process.env.SENTRY_AUTH_TOKEN,
-    }),
-  ],
+  }
 })
